106f573fbedee9bf7467da0ee66802c919ca3eaf
poorvi-s-p <74493537+poorvi-s-p@users.noreply.github.com>
2020-12-10 17:07:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,96 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+var engine,world;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var  side1,side2,side3;
+
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	engine=Engine.create();
+	world=engine.world;
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+	 
+	side3=Bodies.rectangle(535,580,20,140,{isStactic:true});
+	World.add(world,side3);
+
+	side2=Bodies.rectangle(275,580,20,140,{isStactic:true});
+	World.add(world,side2);
+
+	side1=Bodies.rectangle(width/2,640,250,20,{isStactic:true});
+	World.add(world,side1);
+
+
+	side1=createSprite(width/2,640,250,20);
+	side1.shapeColor=("red");
+
+	side2=createSprite(275,580,20,140);
+	side2.shapeColor=("red")
+
+	side3=createSprite(535,580,20,140);
+	side3.shapeColor=("red");
+
+	engine = Engine.create();
+	world = engine.world;
+
+
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.8, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+	
+	Engine.run(engine);
+
+	
+
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+
+ 
+  drawSprites();
+ 
+
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+	Matter.Body.setStatic(packageBody, false)
+	
+ }
+
+}
+
+
